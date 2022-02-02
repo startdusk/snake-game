eb8507ae1360d625cf34058033b072910dc5b9c1
startdusk <522383345@qq.com>
2022-02-03 01:18:55 +0800

    feat✨:  add move one row

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,26 @@ pub struct World {
 
 #[wasm_bindgen]
 impl World {
+    // width: 贪吃蛇游戏界面的宽度
+    // width = 8 界面如下:
+    // 0  1  2  3  4  5  6  7
+    // 8  9  10 11 12 13 14 15
+    // 16 17 18 19 20 21 22 23
+    // 24 25 26 27 28 29 30 31
+    // 32 33 34 35 36 37 38 39
+    // 40 41 42 43 44 45 46 47
+    // 48 49 50 51 52 53 54 55
+    // 56 57 58 59 60 61 62 63
+    // spawn_idx: 贪吃蛇初始化位置(在哪个格子上)
+    // 如初始位置在 11 的位置上
+    // 0  1  2  3  4  5  6  7
+    // 8  9  10 ■  12 13 14 15
+    // 16 17 18 19 20 21 22 23
+    // 24 25 26 27 28 29 30 31
+    // 32 33 34 35 36 37 38 39
+    // 40 41 42 43 44 45 46 47
+    // 48 49 50 51 52 53 54 55
+    // 56 57 58 59 60 61 62 63
     pub fn new(width: usize, spawn_idx: usize) -> World {
         World {
             width,
@@ -64,12 +84,15 @@ impl World {
 
     pub fn update(&mut self) {
         let snake_idx = self.snake_head_idx();
+        let row = snake_idx / self.width;
 
         if self.snake.direction == Direction::Right {
-            self.snake.body[0].0 = (snake_idx + 1) % self.size;
+            let next_col = (snake_idx + 1) % self.width;
+            self.snake.body[0].0 = (row * self.width) + next_col;
         }
         if self.snake.direction == Direction::Left {
-            self.snake.body[0].0 = (snake_idx - 1) % self.size;
+            let next_col = (snake_idx - 1) % self.width;
+            self.snake.body[0].0 = (row * self.width) + next_col;
         }
     }
 }
